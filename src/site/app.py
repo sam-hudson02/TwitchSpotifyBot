f678f4f661905eec5f092077ed25809a23266068
sam-hudson02 <samhudson@outlook.com>
2022-12-15 18:32:16 +0000

    API Update
    - Changed method of checking if bots are running
    - Added endpoints to check on status of bots
    - Added endpoints for restarting and stopping the bot

--- src/site/app.py
+++ src/site/app.py
@@ -8,15 +8,15 @@ path_src = os.path.abspath('./src')
 sys.path.insert(1, path_src)
 import main as bot
 from logger import Log
-from subprocess import call
+from subprocess import Popen
 import json
+import psutil
 
 static_folder = os.path.abspath('./src/site/static')
 app = Flask(__name__)
 
 class Server:
     def __init__(self):
-        self.bot_running = False
         if not os.path.exists('./data'):
             os.mkdir('./data')
         if not os.path.exists('./data/sbotify.log'):
@@ -27,6 +27,7 @@ class Server:
         self.creds = bot.get_creds(self.log)
         self.user = self.creds['spotify username']
         self.cache_path = os.path.abspath(f'./secret/.cache-{self.user}')
+        self.proc = None
         if not os.path.exists(self.cache_path):
             with open(self.cache_path, 'w') as f:
                 json.dump({}, f)
@@ -40,7 +41,7 @@ class Server:
 
     def redirect_to_spotify(self):
         self.log.info("Checking if bot is running...")
-        if self.bot_running:
+        if self.bot_running():
             self.log.info("Bot is running, redirecting to bot running page.")
             return flask.render_template("bot_running.html")
         else:
@@ -50,30 +51,61 @@ class Server:
                                                   redirect_uri=redirect, open_browser=False, scope=self.scopes, cache_handler=self.cache)
             return flask.redirect(self.spot_oath.get_authorize_url())
     
-    
     def spotify_callback(self):
-        if self.bot_running:
+        if self.bot_running():
             return flask.redirect("/")
         self.log.info("Spotify callback received.")
         code = self.spot_oath.parse_response_code(request.url)
         self.spot_oath.get_access_token(code)
         self.log.info("Spotify login successful, starting bot.")
-        if not self.bot_running:
+        if not self.bot_running():
             th.Timer(1, self.start_bots).start()
-            self.bot_running = True
         return flask.render_template("bot_running.html")
     
     def start_bots(self):
         # check if running on windows or linux
+        if self.bot_running():
+            return
         if os.name == 'nt':
             self.log.info("Windows detected, starting bot with python")
-            call(["python", "src/main.py"])
+            self.proc = Popen(["python", "src/main.py"])
         else:
             self.log.info("Linux detected, starting bot with python3")
-            call(["python3", "src/main.py"])
+            self.proc = Popen(["python3", "src/main.py"])
+
+    def bot_running(self):
+        if self.proc is None:
+            return False
+        elif psutil.pid_exists(self.proc.pid):
+            return True
+        else:
+            return False
+
+    def bot_check(self):
+        if self.bot_running():
+            return "", 200
+        else:
+            return "", 503
+
+    def terminate_bot(self):
+        if self.bot_running():
+            self.proc.terminate()
+            self.proc = None
+            return "", 200
+        else:
+            return "", 503
+
+    def restart_bot(self):
+        if self.bot_running():
+            self.proc.terminate()
+            self.proc = None
+            th.Timer(1, self.start_bots).start()
+            return "", 200
+        else:
+            return "", 503
     
     def run(self):
-        app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 if __name__ == "__main__":
@@ -84,6 +116,15 @@ if __name__ == "__main__":
     @app.route("/spotify-redirect", methods=["GET"])
     def route_spotify_redirect():
         return server.spotify_callback()
+    @app.route("/bot-check", methods=["GET"])
+    def route_bot_check():
+        return server.bot_check()
+    @app.route("/terminate-bot", methods=["GET"])
+    def route_terminate_bot():
+        return server.terminate_bot()
+    @app.route("/restart-bot", methods=["GET"])
+    def route_restart_bot():
+        return server.restart_bot()
     @app.route("/static/style.css", methods=["GET"])
     def route_style():
         return flask.send_from_directory(static_folder, "style.css")
