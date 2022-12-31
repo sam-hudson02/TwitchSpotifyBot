e9c94611969081177034cfdad7cff46e8bb5c9cb
sam-hudson02 <samhudson@outlook.com>
2022-12-31 21:25:44 +0000

    Update creds
    NoCreds exception now takes missing cred.

--- src/utils/creds.py
+++ src/utils/creds.py
@@ -14,14 +14,14 @@ class TwitchCreds:
     def token(self):
         if self.__token is None:
             self.log.critical('Twitch token missing! Not continuing.')
-            raise NoCreds
+            raise NoCreds('Twitch Token')
         return self.__token
     
     @property
     def channel(self):
         if self.__channel is None:
             self.log.critical('Twitch channel missing! Not continuing.')
-            raise NoCreds
+            raise NoCreds('Twitch Channel')
         return self.__channel
 
 
@@ -35,22 +35,19 @@ class DiscordCreds:
     @property
     def token(self):
         if self.__token is None:
-            self.log.critical('Discord token missing! Not continuing.')
-            raise NoCreds
+            self.log.critical('Discord token missing!')
         return self.__token
     
     @property
     def queue_channel_id(self):
         if self.__queue_channel_id is None:
-            self.log.critical('Discord queue channel id missing! Not continuing.')
-            raise NoCreds
+            self.log.critical('Discord queue channel id missing!')
         return self.__queue_channel_id
     
     @property
     def leaderboard_channel_id(self):
         if self.__leaderboard_channel_id is None:
-            self.log.critical('Discord leaderboard channel id missing! Not continuing.')
-            raise NoCreds
+            self.log.critical('Discord leaderboard channel id missing!')
         return self.__leaderboard_channel_id
 
     def creds_valid(self) -> bool:
@@ -63,28 +60,28 @@ class SpotifyCreds:
     def __init__(self, log: Log):
         self.log = log
         self.__client_id = getenv('SPOTIFY_CLIENT_ID')
-        self.__client_secret = getenv('SPOTIFY_CLIENT_SECRET')
+        self.__client_secret = getenv('SPOTIFY_SECRET')
         self.__username = getenv('SPOTIFY_USERNAME')
 
     @property
     def client_id(self):
         if self.__client_id is None:
             self.log.critical('Spotify client id missing! Not continuing.')
-            raise NoCreds
+            raise NoCreds('Spotify Client ID')
         return self.__client_id
     
     @property
     def client_secret(self):
         if self.__client_secret is None:
             self.log.critical('Spotify client secret missing! Not continuing.')
-            raise NoCreds
+            raise NoCreds('Spotify Client Secret')
         return self.__client_secret
     
     @property
     def username(self):
         if self.__username is None:
             self.log.critical('Spotify username missing! Not continuing.')
-            raise NoCreds
+            raise NoCreds('Spotify Username')
         return self.__username
 
 
