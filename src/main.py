1959b9e607f94c5ce52b6c2b3810a18395892099
sam-hudson02 <samhudson@outlook.com>
2022-12-31 22:31:08 +0000

    Refactor main.py
    Refactored main.py to work with settings, audio controller, discord bot and twitch bot refactors.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import os
 import threading as th
-from spotify_api import Spotify
-from twitch.main import TwitchBot
-from discord_bot import DiscordBot
+from AudioController.spotify_api import Spotify
+from twitch.twitch_bot import TwitchBot
+from disc.discord_bot import DiscordBot
 from os.path import exists
 from utils.errors import *
-from audio_controller import AudioController, Context
+from AudioController.audio_controller import AudioController, Context
 from utils import Log, DB, Settings, Creds
 
 
@@ -15,7 +15,7 @@ def init_data_dir():
 
 
 def start_twitch_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context):
-    twitch_log = Log('Twitch', settings.get_log())
+    twitch_log = Log('Twitch', settings.log)
 
     db = DB(db_log)
     s_bot = Spotify(creds.spotify)
@@ -32,14 +32,14 @@ def start_twitch_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context
 
 
 def start_discord_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context):
-    discord_log = Log('Discord', settings.get_log())
+    discord_log = Log('Discord', settings.log)
 
     db = DB(db_log)
     s_bot = Spotify(creds.spotify)
 
     ac = AudioController(db, s_bot, ctx)
 
-    d_bot = DiscordBot(creds.discord, creds.twitch.channel, discord_log, s_bot, db, ac)
+    d_bot = DiscordBot(creds.discord, creds.twitch.channel, discord_log, s_bot, db, ac, settings)
     d_bot.run(creds.discord.token)
 
 
@@ -52,12 +52,14 @@ def main():
     settings = Settings(main_log)
     ctx = Context()
 
-    db_log = Log('Database', settings.get_log())
+    # Database log is used for both twitch and discord bots DB instances
+    # Database not initialized in main function as it cannot be shared between threads
+    db_log = Log('Database', settings.log)
 
-    if creds.discord.creds_valid() and settings.get_discord_bot():
+    if creds.discord.creds_valid() and settings.discord_bot:
         th.Thread(target=start_discord_bot, args=(
             db_log, creds, settings, ctx), daemon=True).start()
-    start_twitch_bot(db_log, creds, settings, ctx, settings)
+    start_twitch_bot(db_log, creds, settings, ctx)
 
 
 if __name__ == "__main__":
