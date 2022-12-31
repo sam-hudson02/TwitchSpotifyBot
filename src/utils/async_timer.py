a39cb98e079cb99b604b8cabc87307b79796316a
sam-hudson02 <samhudson@outlook.com>
2022-12-31 21:34:05 +0000

    Add Timer, time_finder and target_finder to utils
    - Moved async timer to its own file in utils.
    - Added async timer, time_finder and target_finder to utils __init__.py

---
+++ src/utils/async_timer.py
@@ -0,0 +1,17 @@
+import asyncio
+
+# creates a timer that calls a function after given number of milliseconds
+class Timer:
+    def __init__(self, timeout: int, callback: callable, args: list = []):
+        # convert milliseconds to seconds
+        self._timeout = float(timeout / 1000)
+        self._callback = callback
+        self._task = asyncio.ensure_future(self._job())
+        self._args = args
+
+    async def _job(self):
+        await asyncio.sleep(self._timeout)
+        await self._callback(*self._args)
+
+    def cancel(self):
+        self._task.cancel()
