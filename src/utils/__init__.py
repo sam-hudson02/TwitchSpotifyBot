da9340615f49c7178b8f03bcf6c2a2cb186a1b81
sam-hudson02 <samhudson@outlook.com>
2023-01-01 18:23:29 +0000

    Add Perms to utils init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,6 @@
 from utils.db_handler import DB
 from utils.logger import Log
-from utils.settings import Settings
+from utils.settings import Settings, Perms
 from utils.creds import Creds, SpotifyCreds, TwitchCreds, DiscordCreds
 from utils.async_timer import Timer
 from utils.twitch_utils import time_finder, target_finder
