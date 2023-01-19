00872cc5810dbfe41839da94b3394f37aabeabde
sam-hudson02 <samhudson@outlook.com>
2023-01-19 11:25:25 +0000

    Tweak to adding req info

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def init_data_dir():
         os.mkdir('./data')
 
 
-def start_twitch_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context):
+def start_twitch_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context, ac_log: Log):
     twitch_log = Log('Twitch', settings.log)
 
     db = DB(db_log)
@@ -27,17 +27,17 @@ def start_twitch_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context
 
     ac = AudioController(db, s_bot, ctx)
 
-    t_bot = TwitchBot(creds.twitch, twitch_log, db, ac, settings)
+    t_bot = TwitchBot(creds.twitch, twitch_log, db, ac, settings, ac_log)
     t_bot.run()
 
 
-def start_discord_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context):
+def start_discord_bot(db_log: Log, creds: Creds, settings: Settings, ctx: Context, ac_log: Log):
     discord_log = Log('Discord', settings.log)
 
     db = DB(db_log)
     s_bot = Spotify(creds.spotify)
 
-    ac = AudioController(db, s_bot, ctx)
+    ac = AudioController(db, s_bot, ctx, ac_log)
 
     d_bot = DiscordBot(creds.discord, creds.twitch.channel, discord_log, s_bot, db, ac, settings)
     d_bot.run(creds.discord.token)
@@ -55,11 +55,12 @@ def main():
     # Database log is used for both twitch and discord bots DB instances
     # Database not initialized in main function as it cannot be shared between threads
     db_log = Log('Database', settings.log)
+    ac_log = Log('AudioController', settings.log)
 
     if creds.discord.creds_valid() and settings.discord_bot:
         th.Thread(target=start_discord_bot, args=(
-            db_log, creds, settings, ctx), daemon=True).start()
-    start_twitch_bot(db_log, creds, settings, ctx)
+            db_log, creds, settings, ctx, ac_log), daemon=True).start()
+    start_twitch_bot(db_log, creds, settings, ctx, ac_log)
 
 
 if __name__ == "__main__":
