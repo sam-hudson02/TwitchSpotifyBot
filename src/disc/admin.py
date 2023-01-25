18a10fbc2bd750a4f06bff032378510ef71b3232
sam-hudson02 <samhudson@outlook.com>
2023-01-25 18:19:19 +0000

    Fixed restart call ip

--- src/disc/admin.py
+++ src/disc/admin.py
@@ -8,6 +8,7 @@ import requests
 
 discord.app_commands.tree
 
+
 class AdminCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -15,7 +16,7 @@ class AdminCog(commands.Cog):
         self.settings: Settings = bot.settings
         self.log: Log = bot.log
         self.db = bot.db
-    
+
     async def cog_before_invoke(self, ctx: discord.Integration) -> None:
         user = ctx.user.name + '#' + ctx.user.discriminator
         if ctx.message is None:
@@ -36,7 +37,6 @@ class AdminCog(commands.Cog):
                 return await func(self, *args, **kwargs)
             return wrapped
         return wrapper
-    
 
     async def cog_app_command_error(self, ctx, error):
         error = getattr(error, 'original', error)
@@ -47,7 +47,7 @@ class AdminCog(commands.Cog):
         await ctx.response.send_message(resp)
         self.log.resp(resp)
         self.log.error(error)
-    
+
     @discord.app_commands.command(name='reset_leaderboard', description='Reset the leaderboard')
     @check()
     async def reset_leaderboard(self, interaction: discord.Interaction):
@@ -61,5 +61,4 @@ class AdminCog(commands.Cog):
         resp = "Restarting..."
         await interaction.response.send_message(content=resp, ephemeral=True)
         self.log.resp(resp)
-        requests.get('http://192.168.1.101:5000/restart-bot')
-    
+        requests.get('http://localhost:5000/restart-bot')
