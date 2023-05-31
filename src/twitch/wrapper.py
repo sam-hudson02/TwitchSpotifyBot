f572334e9b9c6ae92abea29ede5046df930549ee
Sam Hudson <samhudson@outlook.com>
2023-05-31 21:39:01 +0100

    added twitch api client to wrapper

--- src/twitch/wrapper.py
+++ src/twitch/wrapper.py
@@ -5,10 +5,13 @@ import socket
 from twitch.message import Message
 import threading as th
 from utils.logger import Log
+import aiohttp
 
 
 class Wrapper:
-    def __init__(self, creds: TwitchCreds, sock: Optional[socket.socket] = None):
+    def __init__(self, creds: TwitchCreds,
+                 sock: Optional[socket.socket] = None):
+        self.api = API(creds)
         self.log = Log('Socket')
         self.creds = creds
         self._on_message: Callable[[Message], Awaitable[None]] = self.empty
@@ -35,13 +38,14 @@ class Wrapper:
             self.sock.recv(2048).decode("utf-8")
             self.log.info('Socket connected')
             await self._on_join(self.creds.channel)
-        except Exception as e:
+        except Exception as err:
             try:
-                self.sock.close()
+                if self.sock is not None:
+                    self.sock.close()
                 self.sock = None
             except Exception as e:
                 self.log.error(f'Error closing socket: {e}')
-            self.log.error(f"Error connecting to socket: {e}")
+            self.log.error(f"Error connecting to socket: {err}")
             await asyncio.sleep(1.5 ** tries)
             if tries < 5:
                 self.log.info(f"Retrying connection {tries + 1}/5")
@@ -103,3 +107,77 @@ class Wrapper:
         self.log.info('Listening to socket')
         while True:
             await self.read()
+
+
+class API:
+    def __init__(self, creds: TwitchCreds) -> None:
+        self.creds = creds
+        self.log = Log('API')
+        self.base_url: str = 'https://api.twitch.tv/helix/'
+        # use token from creds
+        self._headers: Optional[dict[str, str]] = None
+        self.session = aiohttp.ClientSession()
+        self.channel: str = creds.channel
+        self._channel_id: Optional[str] = None
+
+    async def do_call(self, endpoint: str, params: Optional[dict] = None,
+                      headers: Optional[dict] = None,
+                      base_url: Optional[str] = None):
+        try:
+            if base_url is None:
+                base_url = self.base_url
+            url = base_url + endpoint
+            if params is None:
+                params = {}
+            if headers is None:
+                headers = await self.headers()
+            resp = await self.session.get(url, headers=headers,
+                                          params=params)
+            if resp.status != 200:
+                err = await resp.json()
+                self.log.error(f'Error calling {endpoint}: {err["message"]}')
+                return None
+            return await resp.json()
+        except Exception as err:
+            self.log.error(f'Error calling {endpoint}: {err}')
+            return None
+
+    async def is_follower(self, user_id: str) -> bool:
+        params = {'from_id': user_id, 'to_id': self.channel_id}
+        resp = await self.do_call('users/follows', params)
+        if resp is None:
+            return False
+        return len(resp['data']) > 0
+
+    async def is_live(self) -> bool:
+        channel_id = await self.channel_id()
+        params = {'user_id': channel_id}
+        resp = await self.do_call('streams', params)
+        if resp is None:
+            return False
+        return len(resp['data']) > 0
+
+    async def get_channel_id(self, channel_name: str) -> Optional[str]:
+        params = {'login': channel_name}
+        resp = await self.do_call('users', params)
+        if resp is None:
+            return None
+        return resp['data'][0]['id']
+
+    async def channel_id(self) -> str:
+        if self._channel_id is None:
+            self._channel_id = await self.get_channel_id(self.channel)
+            if self._channel_id is None:
+                raise Exception('Could not get channel id')
+        return self._channel_id
+
+    async def headers(self) -> dict[str, str]:
+        if self._headers is None:
+            headers = {'Authorization': f'Bearer {self.creds.token}'}
+            resp = await self.do_call('validate', headers=headers,
+                                      base_url='https://id.twitch.tv/oauth2/')
+            if resp is None:
+                raise Exception('Could not get client id')
+            headers['Client-Id'] = resp['client_id']
+            self._headers = headers
+        return self._headers
