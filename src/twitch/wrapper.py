b0424ff284f70eaf4e917b458dcaf9b476706cb2
Sam <samhudson@outlook.com>
2023-05-31 14:00:52 +0100

    Logging and better error handling on socket connect

--- src/twitch/wrapper.py
+++ src/twitch/wrapper.py
@@ -4,10 +4,12 @@ from typing import Awaitable, Callable, Optional
 import socket
 from twitch.message import Message
 import threading as th
+from utils.logger import Log
 
 
 class Wrapper:
     def __init__(self, creds: TwitchCreds, sock: Optional[socket.socket] = None):
+        self.log = Log('Socket')
         self.creds = creds
         self._on_message: Callable[[Message], Awaitable[None]] = self.empty
         self._on_live: Callable[[None], Awaitable[None]] = self.empty
@@ -17,32 +19,42 @@ class Wrapper:
         self.sock: Optional[socket.socket] = sock
 
     async def empty(self, *args, **kwargs):
-        print("empty")
         pass
 
     async def connect(self, tries: int = 0):
-        if tries > 5:
-            return
         try:
             if self.sock is None:
                 self.sock = socket.socket()
+            self.log.info(f"Connecting to {self.server}:{self.port}")
             self.sock.connect((self.server, self.port))
             self.sock.send('CAP REQ :twitch.tv/membership twitch.tv/tags\n'
                            .encode("utf-8"))
             self.sock.send(f"PASS oauth:{self.creds.token}\n".encode("utf-8"))
             self.sock.send(f"NICK {self.creds.bot_name}\n".encode("utf-8"))
             self.sock.send(f"JOIN #{self.creds.channel}\n".encode("utf-8"))
-            resp = self.sock.recv(2048).decode("utf-8")
-            print(resp)
+            self.sock.recv(2048).decode("utf-8")
+            self.log.info('Socket connected')
             await self._on_join(self.creds.channel)
         except Exception as e:
-            print(e)
+            try:
+                self.sock.close()
+                self.sock = None
+            except Exception as e:
+                self.log.error(f'Error closing socket: {e}')
+            self.log.error(f"Error connecting to socket: {e}")
             await asyncio.sleep(1.5 ** tries)
-            await self.connect(tries + 1)
+            if tries < 5:
+                self.log.info(f"Retrying connection {tries + 1}/5")
+                await self.connect(tries + 1)
+            else:
+                self.log.error('Max retries reached')
+                # exit entire program
+                exit(1)
 
     def disconnect(self):
         if self.sock is None:
             return
+        self.log.info('Disconnecting socket')
         self.sock.send(f"PART #{self.creds.channel}\n".encode("utf-8"))
         self.sock.close()
 
@@ -79,11 +91,8 @@ class Wrapper:
 
     async def read(self):
         if self.sock is None:
-            print("no socket")
             return
-        print("listening")
         resp = self.sock.recv(2048).decode("utf-8")
-        print(resp)
         if resp.startswith("PING"):
             self.sock.send("PONG\n".encode("utf-8"))
         elif len(resp) > 0 and self.is_message(resp):
@@ -91,6 +100,6 @@ class Wrapper:
             await self._on_message(msg)
 
     async def listen(self):
-        print("connected")
+        self.log.info('Listening to socket')
         while True:
             await self.read()
